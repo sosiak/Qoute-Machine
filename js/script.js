520cf24122fbde49e234d4882a310ffe88f8beba
Slawomir Osiak <slawek-osiak@o2.pl>
2017-06-24 17:49:55 +0200

    add

---
+++ js/script.js
@@ -0,0 +1,36 @@
+var tweetLink = "https://twitter.com/intent/tweet?text=";
+var prefix = "https://cors-anywhere.herokuapp.com/";
+var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+
+function getQuote() {
+    $.getJSON(prefix + quoteUrl, createTweet);
+}
+
+function createTweet(input) {
+    var data = input[0];
+
+    var quoteText = $(data.content).text().trim();
+    var quoteAuthor = data.title;
+
+    if (!quoteAuthor.length) {
+        quoteAuthor = "Unknown author";
+    }
+    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
+
+    if (tweetText.length > 140) {
+    getQuote();
+	} else {
+    var tweet = tweetLink + encodeURIComponent(tweetText);
+    $('.quote').text(quoteText);
+    $('.author').text("- " + quoteAuthor);
+    $('.tweet').attr('href', tweet);
+}
+
+$.ajaxSetup({ cache: false });
+}
+$(document).ready(function() {
+    getQuote();
+    $('.trigger').click(function() {
+        getQuote();
+    })
+});
