827d9d15b667269b2c4e2a402b528a1427f9880b
Slawomir Osiak <slawek-osiak@o2.pl>
2017-06-24 22:17:49 +0200

    add

--- js/script.js
+++ js/script.js
@@ -1,36 +1,33 @@
-var tweetLink = "https://twitter.com/intent/tweet?text=";
-var prefix = "https://cors-anywhere.herokuapp.com/";
-var quoteUrl = "https://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1";
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+var country = $('#country');
 
-function getQuote() {
-    $.getJSON(prefix + quoteUrl, createTweet);
+$('#search').click(searchCountries);
+
+function searchCountries() {
+    var countryName = $('#country-name').val();
+    if(!countryName.length) countryName = 'Poland';
+    $.ajax({
+        url: url + countryName,
+        method: 'GET',
+        success: showCountriesList
+    });
 }
-
-function createTweet(input) {
-    var data = input[0];
-
-    var quoteText = $(data.content).text().trim();
-    var quoteAuthor = data.title;
-
-    if (!quoteAuthor.length) {
-        quoteAuthor = "Unknown author";
-    }
-    var tweetText = "Quote of the day - " + quoteText + " Author: " + quoteAuthor;
-
-    if (tweetText.length > 140) {
-    getQuote();
-	} else {
-    var tweet = tweetLink + encodeURIComponent(tweetText);
-    $('.quote').text(quoteText);
-    $('.author').text("- " + quoteAuthor);
-    $('.tweet').attr('href', tweet);
+function showCountriesList(resp) {
+  countriesList.empty();
+  resp.forEach(function(item){
+    $('<li>').text(item.name).appendTo(countriesList);
+    $('<div class="col-xs-6 country-left">').text("Capital City: ").appendTo(countriesList);
+    $('<div class="col-xs-6 country-right">').text(item.capital).appendTo(countriesList);
+    $('<div class="col-xs-6 country-left">').text("Languages: ").appendTo(countriesList);
+    $('<div class="col-xs-6 country-right">').text(item.languages).appendTo(countriesList);
+    $('<div class="col-xs-6 country-left">').text("Curriences: ").appendTo(countriesList);
+    $('<div class="col-xs-6 country-right">').text(item.currencies).appendTo(countriesList);
+    $('<div class="col-xs-6 country-left">').text("Languages: ").appendTo(countriesList);
+    $('<div class="col-xs-6 country-right">').text(item.languages).appendTo(countriesList);
+    $('<div class="col-xs-6 country-left">').text("Timezone: ").appendTo(countriesList);
+    $('<div class="col-xs-6 country-right">').text(item.timezones).appendTo(countriesList);
+    $('<div class="col-xs-6 country-left">').text("Region: ").appendTo(countriesList);
+    $('<div class="col-xs-6 country-right">').text(item.region).appendTo(countriesList);
+    });
 }
-
-$.ajaxSetup({ cache: false });
-}
-$(document).ready(function() {
-    getQuote();
-    $('.trigger').click(function() {
-        getQuote();
-    })
-});
